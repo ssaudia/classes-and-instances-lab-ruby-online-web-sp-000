afc480e3fdd71ebe2f83a24d4e57ef4a0cfb9b08
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-16 21:14:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,6 @@
+# lib/person.rb
 class Person
-  def talk
-    puts "Hello World!"
-  end
-  def walk
-    puts "The Person is walking"
-  end
 end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
